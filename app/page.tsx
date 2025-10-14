76eadd7cb465437e078b4d12bf9e05ca942945a2
UmerKhan1998 <devUk9298@gmail.com>
2025-10-13 21:23:24 -0700

    push

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,9 @@ import CrosswordPuzzle from '@/components/CrosswordPuzzle'
 import LocateItComp from '@/components/LocateItComp'
 
 const page = () => {
-  const wordsInput = ["NASA", "ABLE", "ECHO", "HEAT", "APPLE", "RABBIT", "BIRD", "MINT", "TOPIC", "TREE"];
-
   return (
     <div>
-      {/* <LocateItComp /> */}
+      <LocateItComp />
       <CrosswordPuzzle />
     </div>
   )
