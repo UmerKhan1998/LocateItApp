ea1a3337388602095e2e9e295953d29f4542d291
UmerKhan1998 <devUk9298@gmail.com>
2025-11-20 11:09:16 +0500

    push

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,9 @@ const MazeComp = dynamic(() => import("@/components/MazeComp"), {
 const WordDiscovery = dynamic(() => import("@/components/WordDiscovery"), {
   ssr: false,
 });
+const CropImageComp = dynamic(() => import("@/components/CropImageComp"), {
+  ssr: false,
+});
 const WordDidscoverYPayload = dynamic(
   () => import("@/components/WordDidscoveryPayload"),
   {
@@ -31,7 +34,7 @@ const page = () => {
       {/* <LocateItComp />
       <MazeComp /> */}
       {/* <CrosswordPuzzle /> */}
-      <FillInTheBlanks
+      {/* <FillInTheBlanks
         text={`By those [b1] sent forth in succession, those that blow violently, 
          and scatter [b2] far and wide, And by those angels who bring criterion [b3]; 
          ... Woe on that Day to the deniers [b4]!`}
@@ -41,7 +44,8 @@ const page = () => {
           { id: "b3", correct: "1" },
           { id: "b4", correct: "3" },
         ]}
-      />
+      /> */}
+      <CropImageComp />
       {/* <WordDiscovery /> */}
       {/* <WordDidscoverYPayload /> */}
     </>
